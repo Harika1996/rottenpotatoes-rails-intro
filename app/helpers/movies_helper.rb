a1ffba2ba683cef2acc2fa67dcbe300801d19aac
EC2 Default User <ec2-user@ip-172-31-15-131.us-east-2.compute.internal>
2018-09-25 23:18:25 +0000

    Part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,14 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    link_to title, :sort => column
+  end
+  def helper_class(field)
+    if(params[:sort].to_s == field)
+      return 'hilite'
+    end
+    return nil
+  end
 end
